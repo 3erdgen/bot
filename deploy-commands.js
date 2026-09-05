aa6f11a3ba53551dc08b1cba2e98da14bc2d2556
3erdgen <3erdgen@gmail.com>
2026-09-04 20:45:28 -0500

    Make GUILD_ID optional and support global registration

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,10 +5,10 @@ const { REST, Routes } = require('discord.js');
 
 const token = process.env.DISCORD_TOKEN;
 const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
+const guildId = process.env.GUILD_ID; // optional
 
-if (!token || !clientId || !guildId) {
-  console.error('DISCORD_TOKEN, CLIENT_ID and GUILD_ID must be set in environment');
+if (!token || !clientId) {
+  console.error('DISCORD_TOKEN and CLIENT_ID must be set in environment');
   process.exit(1);
 }
 
@@ -25,10 +25,17 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
-    console.log(`Registering ${commands.length} commands to guild ${guildId}`);
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
-    console.log('Commands registered.');
+    if (guildId) {
+      console.log(`Registering ${commands.length} commands to guild ${guildId}`);
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+      console.log('Guild commands registered.');
+    } else {
+      console.log(`Registering ${commands.length} global commands`);
+      await rest.put(Routes.applicationCommands(clientId), { body: commands });
+      console.log('Global commands registered. Note: global propagation can take up to 1 hour.');
+    }
   } catch (err) {
     console.error('Error registering commands:', err);
+    process.exit(1);
   }
 })();
